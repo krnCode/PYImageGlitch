8c6508b2de54bcdd773c59784e7463b1bbd2de81
kornflakes <61383309+kornflakes@users.noreply.github.com>
2020-05-10 16:22:35 -0300

    Script for image glitch

---
+++ PYImageGlitch.py
@@ -0,0 +1,35 @@
+from glitch_this import ImageGlitcher
+from PIL import Image
+import os
+from datetime import datetime
+
+time_start = datetime.now()
+
+# 1 - Defining a variable to use the glitch module
+glitcher = ImageGlitcher()
+
+# 2 - Defining the paths to get the images and output the glitched images
+# 2.1 - Path of raw images
+rawDir = r'C:\Users\conta\Pictures\Wallpapers'
+# 2.2 - Path of glitched images (output)
+modDir = r'C:\Users\conta\Pictures\WallsConverted'
+
+# 3 - Making sure that the output folder exists
+if os.path.exists(modDir) == False:
+    os.makedirs(r'C:\Users\conta\Pictures\WallsConverted')
+
+# 4 - Glitch the images of the desired folder
+for rawImage in os.listdir(rawDir):
+    start = datetime.now()
+    img = Image.open(rawDir + "/" + str(rawImage))
+    rgbImg = img.convert('RGB')
+    glitchImage = glitcher.glitch_image(rgbImg, 6.8, color_offset=True)
+    glitchImage.save(modDir + '/' + rawImage[:-4] + '-glitch.jpg')
+    end = datetime.now()
+    print(f'{rawImage} done in {end - start}!')
+
+time_end = datetime.now()
+
+# 5 - Print the progress of the glitching
+print(f'All done! The process took {time_end - time_start} in total to finish.' '\n'
+      'Check the folder to see your glitched images')
